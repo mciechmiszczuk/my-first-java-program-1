ca131d1f84e1de74fe7c3f0c13c3fcb09108dc11
astachurski <astachurski@gmail.com>
2017-04-20 18:19:05 +0200

    my first commit

---
+++ src/main/java/esere/com/App.java
@@ -0,0 +1,31 @@
+package esere.com;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println("ts");
+
+        }
+
+        for (int i = 0; i < 22;
+             i++) {
+
+        }
+
+        for (int i = 0;
+             i < 23; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Hello  enie nie" +
+                "enienienn " +
+                "Wo" +
+                "rld! enetrsnet" +
+                "rnt rtrstrstrt  " +
+                "srttrstrst");
+    }
+}
